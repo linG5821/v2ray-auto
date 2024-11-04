09d2d510834b8e7a15a260a9afb8bee1dba9f06d
lsj <lishaojie@aithu.com>
2024-11-04 14:30:10 +0800

    更新配置

--- src/v2ray_auto.py
+++ src/v2ray_auto.py
@@ -271,7 +271,7 @@ def main():
        log('proxy available...')
        return
 
-    area_filter = r"香港|台湾|新加坡"
+    area_filter = r"台湾|新加坡"
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
