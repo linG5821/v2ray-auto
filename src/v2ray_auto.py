dd64cee9f787ffd5b5fbf39f066b52177a62c269
lsj <lishaojie@aithu.com>
2026-02-25 17:27:11 +0800

    修改为最新的请求方式，增加请求的UA标识

--- src/v2ray_auto.py
+++ src/v2ray_auto.py
@@ -290,7 +290,7 @@ def main():
 
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        "User-Agent": "V2rayNG"
     }
 
     try:
